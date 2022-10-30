efbea7834925d5efb0392dad866f7344576898a3
= <=>
2022-10-30 20:22:45 +0300

    Lecture 19

---
+++ src/test/java/lecture19/hw/MySite.java
@@ -0,0 +1,12 @@
+package lecture19.hw;
+
+import org.testng.annotations.Test;
+
+public class MySite {
+
+    @Test
+    public void MySiteTest(){
+       driver.get("file:////Users/veronikab/Downloads/My Site");
+    }
+
+}
