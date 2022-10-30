23610bb5a70eb35e467ad62512d53695f807839c
= <=>
2022-10-30 20:41:11 +0300

    My Site

--- src/test/java/lecture19/hw/MySite.java
+++ src/test/java/lecture19/hw/MySite.java
@@ -1,12 +1,11 @@
 package lecture19.hw;
-
 import org.testng.annotations.Test;
 
 public class MySite {
 
     @Test
     public void MySiteTest(){
-       driver.get("file:////Users/veronikab/Downloads/My Site");
+        driver.get("/Users/veronikab/IdeaProjects/Automation_QA16/src/test/java/lecture19/hw/My_Site/Index.html");
     }
 
 }
